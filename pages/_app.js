e11651640e01b08959600bef28521b5d9605905c
unknown65182 <unknown65182@gmail.com>
2021-03-22 02:50:03 +0300

    disable google next fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import "normalize.css";
 import "../src/styles/globals.css";
 import Head from "next/head";
-import { GoogleFonts } from "next-google-fonts";
 
 function Toxin({ Component, pageProps }) {
   return (
     <>
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" />
-      <GoogleFonts href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       <Head>
         <meta charSet="utf-8" />
         <meta
@@ -15,9 +12,17 @@ function Toxin({ Component, pageProps }) {
           content="width=device-width, initial-scale=1, maximum-scale=5 shrink-to-fit=no"
         />
         <meta name="description" content="Toxin web store" />
-
         <link rel="canonical" href="https://toxin.vercel.app" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </>
